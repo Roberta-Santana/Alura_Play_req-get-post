88c75b1eaff4c035c8f6de3c5f86f56aafda6662
Roberta Santana <g.roberta.s@hotmail.com>
2024-06-21 21:03:54 -0300

    listarVideos e criarVideos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,10 +1,29 @@
-async function listaVideos(){
+async function listarVideos(){
     const conexao = await fetch('http://localhost:3000/videos');
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
 }
 
+async function criarVideos(titulo, descricao, url, imagem){
+    //estrutura de uma função assíncrona, que faz uma conexão com a API 
+    const conexao = await fetch('http://localhost:3000/videos', {
+        //e cria uma requisição POST,
+        method:"POST",
+        headers:{ "Content-type":"application/json"},
+        body: JSON.stringify({
+            titulo:titulo,
+            descricao:`${descricao} mil visualizações`,
+            url: url,
+            imagem:imagem})
+    })
+    //que é a de cadastrar novas coisas lá na nossa API, no nosso db.json
+    const conexaoConvertida = conexao.json();
+    return conexaoConvertida;
+}
+
+
 export const conectaApi = {
-    listaVideos
+    listarVideos,
+    criarVideos
 }
--- js/mostraVideos.js
+++ js/mostraVideos.js
@@ -1,22 +1,30 @@
-import { conectaApi } from "./conectaApi";
+import { conectaApi } from "./conectaApi.js";
 
-const lista = document.querySelector('[data-lista]');
+const listaHtml = document.querySelector('[data-lista]');
 
-function constroiCard(){
+function constroiCard(titulo, descricao, url, imagem){ //cria outros itens da lista do index.html
     const video = document.createElement('li');
     video.className = 'videos__item';
-    video.innerHTML = `<iframe width="100%" height="72%" src="https://www.youtube.com/embed/pA-EgOaF23I"
-                title="YouTube video player" frameborder="0"
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+                title="${titulo}" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
             <div class="descricao-video">
-                <img src="./img/logo.png" alt="logo canal alura">
-                <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
-                <p>236 mil visualizações</p>
+                <img src="${imagem}" alt="logo canal alura">
+                <h3>${titulo}</h3>
+                <p>${descricao}</p>
             </div>`;
     return video;
 }
 
-async function listaVideos(){
-    const lista = await conectaApi.listaVideos();
+async function listarVideos(){ 
+    //Acessa a conectaApi.js, pega a função listaVideos e retorna o valor da nossa API, do db.json via fetch; 
+    const listaApi = await conectaApi.listarVideos();
+    //para cada item da lista da nossa API, um item da lista do index.html deve ser criado
+    listaApi.forEach(elemento => listaHtml.appendChild(//cada elemento que vai pra listaApi, é a criação de um elemento filho da listaHtml, onde( 
+        //usando a função ConstroiCard(
+        //cada item criado deve ter os atributos que já estão no db.json(colunas da tabela do banco- titulo, descrição, url, imagem)
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 }
+
+listarVideos();
